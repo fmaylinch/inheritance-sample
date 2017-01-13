ca8295cae36527890afd7998e65498c3979b1f03
fmaylinch <ferranmaylinch@gmail.com>
2017-01-13 18:34:51 +0100

    Use a list of machines

--- src/main/java/com/codethen/inheritance/Main.java
+++ src/main/java/com/codethen/inheritance/Main.java
@@ -1,13 +1,35 @@
 package com.codethen.inheritance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		Fridge fridge = new Fridge();
-		Oven oven = new Oven();
+		Machine fridge = new Fridge();
+		Machine fridge2 = new Fridge();
+		Machine oven = new Oven();
+		Machine oven2 = new Oven();
 
-		fridge.turnOn();
-		oven.turnOn();
+		// oven.increaseTemp(20);
+
+		List<Machine> list = new ArrayList<>();
+		list.add(oven);
+		list.add(oven2);
+		list.add(fridge);
+		list.add(fridge2);
+
+/*
+		for (int i=0; i < list.size(); i++) {
+			Machine machine = list.get(i);
+			System.out.print(i + " - ");
+			machine.turnOn();
+		}
+*/
+
+		for (Machine machine : list) {
+			machine.turnOn();
+		}
 	}
 }
--- src/main/java/com/codethen/inheritance/Oven.java
+++ src/main/java/com/codethen/inheritance/Oven.java
@@ -2,7 +2,17 @@ package com.codethen.inheritance;
 
 public class Oven implements Machine {
 
+	private int temperature;
+
+	public Oven() {
+		this.temperature = 0;
+	}
+
 	public void turnOn() {
 		System.out.println("Oven is on");
+	}
+
+	public void increaseTemp(int inc) {
+		this.temperature += inc;
 	}
 }
